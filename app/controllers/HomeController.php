c9e7aeecd994c5996f6e4a00e2f93e68e35d9e20
miceica <miguelceica23@gmail.com>
2024-06-10 18:56:14 +0200

    ajax

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -6,9 +6,9 @@ class HomeController
 {
     public function index()
     {
+        
         // Cargar la vista
         require '../app/views/home/index.php';
     }
 }
-
-
+?>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Home</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
     <div class="container mt-5">
         <h1>Home</h1>
         <p>Bienvenido a nuestra aplicación web MVC en PHP.</p>
+
+        <input type="text" id="texto" placeholder="Introduce tu texto aquí">
+        <button id="btnajax">Pulsar</button>
     </div>
+    <script src="assets/js/home.js"></script>
 </body>
+
 </html>
